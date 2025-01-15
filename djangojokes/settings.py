b86b3a145de5cc4532ed66aba1fa4dd318f23d2a
ZER <51429678+ericcreencia25@users.noreply.github.com>
2025-01-15 14:54:47 +0800

    Exercise 22

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'jokes',
+    'USER': 'postgres',
+    'PASSWORD': '3mb_P@ssw0rd',
+    'HOST': 'localhost',
+    'PORT': 5432
     }
 }
 
